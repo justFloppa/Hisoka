10e1e36ff8bfbc195e7783ab5ecd0baf7d1c1433
Floppa <arrrslan1405@gmail.com>
2026-03-03 16:09:46 +0500

    realised persistent background server

--- index.js
+++ index.js
@@ -3,77 +3,54 @@ import b4a from 'b4a'
 import process from 'bare-process'
 import readline from 'bare-readline' 
 
-
-function readInput(prompt, callback) {
-  console.log(prompt)
-  process.stdin.once('data', (data) => {
-    const input = data.toString().trim()
-    callback(input)
-  })
-}
-
 const dht = new DHT() //хэш таблица
 
 //сервер
 async function runServer() {
+    const keyPair = DHT.keyPair() //public и private ключи создание
+
     const server = dht.createServer(conn => { //сервер создание
-    console.log('server started')
+    console.log('к вам подключились')
     process.stdin.pipe(conn).pipe(process.stdout) //отображение своих и чужих соо
     })
 
-    const keyPair = DHT.keyPair() //public и private ключи создание
     server.listen(keyPair).then(() => { //сервер слушает
-    console.log('public key: ', b4a.toString(keyPair.publicKey, 'hex')) //наш ключ
+    console.log('сервер запущен\npublic key: ', b4a.toString(keyPair.publicKey, 'hex')) //наш ключ
+    console.log("введите ключ или ждите подключения по вашему: ")
 })
 
     Pear.teardown(() => server.close())
 }
 
-async function runClient() {
-    console.log("введите публичный ключ: ")
-    const input = await new Promise(resolve => {
-            process.stdin.once('data', (data) => {
-                resolve(data.toString().trim())
-            })
-        })
-    let key = input
-
-
-    //const key = Pear.config.args[0] //берет ключ собеседника
-    //if (!key) throw new Error('provide a key') //просит ключ
+async function runClient(key) {
     
     console.log('Connecting to:', key) //подключаемся по ключу
     const publicKey = b4a.from(key, 'hex') //конверт ключа в бин
     
     const conn = dht.connect(publicKey) //создаем соединение используя таблицу и наш ключ
-    conn.once('open', () => console.log('got connection!')) //подключаемся
+    conn.once('open', () => console.log('подключено')) //подключаемся
     
     process.stdin.pipe(conn).pipe(process.stdout) //выводим сообщения собеседника и наши
 }
 
-const rl = readline.createInterface({
+async function main() {
+  const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+  terminal: true
+  })
 
-console.log("1-сервер\n2-клиент")
+  await runServer()
 
-
-readInput('\nВыберите режим: ', (choice) => {
-  switch(choice) {
-    case '1':
-      runServer()
-      break
-    case '2':
-      runClient()
-      break
-    default:
-      console.log('попробуйте еще раз')
-      process.exit(1)
+  rl.on('data', (line) => {
+  if (line.startsWith('/')) {
+    runClient(line.slice(1))
   }
-})
+  })
+}
+
+main()
 
 Pear.teardown(() => {
   dht.destroy()
 })
-
