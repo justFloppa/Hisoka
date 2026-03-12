ff6f6b9c1d96341ba5a243fd1f03d6b9442dd4a8
Floppa <arrrslan1405@gmail.com>
2026-03-12 17:54:29 +0500

    added bare-kit in gradle files, moved js to app/src/main/assets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("com.pears.bare:barekit:0.0.6")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
